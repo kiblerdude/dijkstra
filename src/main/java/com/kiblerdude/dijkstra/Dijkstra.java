917d6a18cb058b30a47449cd4fa93290f17c1c35
kiblerdude <kiblerj@gmail.com>
2015-11-18 21:37:22 -0500

    progress

--- src/main/java/com/kiblerdude/dijkstra/Dijkstra.java
+++ src/main/java/com/kiblerdude/dijkstra/Dijkstra.java
@@ -15,65 +15,73 @@ import com.google.common.collect.Sets;
  * Dijkstra's Shortest Path Algorithm
  *
  */
-public class Dijkstra 
-{
+public class Dijkstra {
 	private static final int NO_PATH_LENGTH = 1000000;
-	public Dijkstra() {
-		
-	}
-	
-	public Integer shortestPath(Graph g, Integer from, Integer to) {
-		Set<Integer> processed = Sets.newHashSet();
-		Integer[] distances = new Integer[g.nodes.size() + 1];
-		
-		Arrays.fill(distances, NO_PATH_LENGTH);
-		
-		distances[from] = 0;
-		
-		Integer v = from;
-		
-		while (!processed.contains(to)) {
-			Node node = g.nodes.get(v);
-			// find the edge with the minimum value
-			// TODO filter edges already processed
-			
-			Optional<Entry<Integer, Integer>> minEdge = node.outgoing.entrySet().stream().min(
-					(p1,p2) -> Integer.compare(p1.getValue(), p2.getValue()));
-			
-			if (minEdge.isPresent()) {
-				System.out.println(minEdge.get());			
-				processed.add(minEdge.get().getKey());
 
-				v =	minEdge.get().getValue();
-			} else {
-				// no path
-				System.out.println("no path");
-				processed.add(to);
-			}
-		}		
-		
+	private final Graph graph;
+
+	public Dijkstra(Graph graph) {
+		this.graph = graph;
+	}
+
+	public Integer shortestPath(Integer from, Integer to) {
+		Set<Integer> processed = Sets.newHashSet();
+		Set<Integer> unprocessed = Sets.newHashSet(graph.nodes.keySet());
+
+		Integer[] distances = new Integer[graph.nodes.size() + 1];
+
+		Arrays.fill(distances, 0);
+
+		Integer n = from;
+		processed.add(n);
+		unprocessed.remove(n);
+
+		// while (!processed.contains(to)) {
+		getShortestEdge(processed, unprocessed, distances);
+		// find the edge with the minimum value from processed nodes to
+		// unprocessed nodes
+		// TODO filter edges already processed
+
+		// }
+
 		return NO_PATH_LENGTH;
 	}
-	
-    public static void main( String[] args ) throws IOException
-    {
-        //Files.newBufferedReader(Paths.get("dijkstraData.txt"));
-        BufferedReader reader = Files.newBufferedReader(Paths.get("dijkstraTest.txt"));
-        Graph graph = new Graph();
-        
-        reader.lines().forEach(line -> {
-        	String[] vertexData = line.split("\\t");
-        	Integer vertex = Integer.parseInt(vertexData[0]);        	
-        	for (int i = 1; i < vertexData.length; i++) {
-        		String[] edgeData = vertexData[i].split(",");
-        		Integer to = Integer.parseInt(edgeData[0]);
-        		Integer length = Integer.parseInt(edgeData[1]);
-        		graph.addEdge(vertex, to, length);
-        	}
-        });
-        
-        System.out.println(graph);
-        
-        new Dijkstra().shortestPath(graph, 1, 4);
-    }
+
+	public Optional<Node> getShortestEdge(Set<Integer> processed, Set<Integer> unprocessed, Integer[] distances) {
+
+
+		processed.stream().forEach(p -> {
+			Node from = graph.nodes.get(p);
+			Optional<Entry<Integer, Integer>> edge = from.outgoing.entrySet().stream()
+				.filter(v -> unprocessed.contains(v.getKey()))
+				.min((e1, e2) -> Integer.compare(distances[p] + e1.getValue(), distances[p] + e2.getValue()));
+			
+			if (edge.isPresent()) {
+				System.out.println(edge.get());
+			}
+		});
+		
+		return Optional.empty();
+	}
+
+	public static void main(String[] args) throws IOException {
+		// Files.newBufferedReader(Paths.get("dijkstraData.txt"));
+		BufferedReader reader = Files.newBufferedReader(Paths.get("dijkstraTest.txt"));
+		Graph graph = new Graph();
+
+		reader.lines().forEach(line -> {
+			String[] vertexData = line.split("\\t");
+			Integer vertex = Integer.parseInt(vertexData[0]);
+			for (int i = 1; i < vertexData.length; i++) {
+				String[] edgeData = vertexData[i].split(",");
+				Integer to = Integer.parseInt(edgeData[0]);
+				Integer length = Integer.parseInt(edgeData[1]);
+				graph.addEdge(vertex, to, length);
+			}
+		});
+
+		System.out.println(graph);
+
+		new Dijkstra(graph).shortestPath(1, 4);
+	}
 }
